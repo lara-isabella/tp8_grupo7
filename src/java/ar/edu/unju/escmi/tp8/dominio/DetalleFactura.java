6d107014108ebd8a6349c731a095b5b341971305
lara-isabella <laraisabellaregina@gmail.com>
2025-11-07 22:19:47 -0300

    Avances Clase DetalleFactura [Lara Colazo]

--- src/java/ar/edu/unju/escmi/tp8/dominio/DetalleFactura.java
+++ src/java/ar/edu/unju/escmi/tp8/dominio/DetalleFactura.java
@@ -1,5 +1,41 @@
 package java.ar.edu.unju.escmi.tp8.dominio;
 
 public class DetalleFactura {
+    private long id;
+    private int cantidad;
+    private double subtotal;
 
+    // CONSTRUCTORES
+
+    public DetalleFactura() {
+    }
+
+    public DetalleFactura(long id, int cantidad, double subtotal) {
+        this.id = id;
+        this.cantidad = cantidad;
+        this.subtotal = subtotal;
+    }
+
+    // GETTERS Y SETTERS
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    public double getSubtotal() {
+        return subtotal;
+    }
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    // METODOS
 }
